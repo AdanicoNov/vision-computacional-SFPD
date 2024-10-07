7a3a927ea5e82f7909b132bbe206c54eb8366b88
AdanicoNov <141366572+AdanicoNov@users.noreply.github.com>
2024-10-06 19:35:43 -0600

    Add files via upload

---
+++ Practicas/Practica4/Practica4/filters.py
@@ -0,0 +1,91 @@
+import numpy as np
+import cv2
+from itertools import permutations
+import math
+class filters:
+    prewitt_x = np.array((-1,0,1,-1,0,1,-1,0,1)).reshape(3, 3)
+    prewitt_y = np.array((-1,-1,-1,0,0,0,1,1,1)).reshape(3, 3)
+    sobel_x = np.array((-1,0,1,-2,0,2,-1,0,1)).reshape(3, 3)
+    sobel_y = np.array((-1,-2,-1,0,0,0,1,2,1)).reshape(3, 3)
+    laplace = np.array((-1,-1,-1,-1,8,-1,-1,-1,-1)).reshape(3, 3)
+    def __init__(self):
+        pass
+        
+    
+
+    def salt_pepper_noise(image, salt_prob=0.01, pepper_prob=0.01):
+       
+        noisy_image = np.copy(image)
+        
+        # Añadir ruido de sal (píxeles blancos)
+        num_salt = np.ceil(salt_prob * image.size)
+        coords_salt = [np.random.randint(0, i - 1, int(num_salt)) for i in image.shape]
+        noisy_image[coords_salt[0], coords_salt[1]] = 1  # Para imágenes en escala de grises
+
+        # Añadir ruido de pimienta (píxeles negros)
+        num_pepper = np.ceil(pepper_prob * image.size)
+        coords_pepper = [np.random.randint(0, i - 1, int(num_pepper)) for i in image.shape]
+        noisy_image[coords_pepper[0], coords_pepper[1]] = 0  # Para imágenes en escala de grises
+
+        return noisy_image
+    
+    def gauss_vector(self, l):
+        """
+        Args:
+            l (int): level in the gauss pyramid
+        """
+        gauss_1d = np.zeros(l+1)
+        for x in range(l+1):
+            gauss_1d[x] = math.factorial(l)/(math.factorial(x)*math.factorial(l-x))
+        return gauss_1d
+
+    def gauss_matrix(self,l):
+        gauss_1d = filters.gauss_vector(self, l)
+        filter_gauss = np.outer(gauss_1d, np.transpose(gauss_1d))
+        scalar = np.sum(filter_gauss)
+        return filter_gauss/scalar
+    
+    def to_matrix(self, vector1, vector2):
+        filter_mat = np.outer(vector1, vector2)
+        scalar = np.sum(filter_mat)
+        return filter_mat
+    
+    def derv1_gauss(self,l):
+        gauss_d = filters.gauss_vector(self, l)
+        gauss_1d = np.zeros((l+1))
+        gauss_1d = np.convolve(gauss_d,(1,-1))
+        print(gauss_1d)
+        return gauss_1d
+        
+    def derv2_gauss(self,l):
+        gauss_1d = filters.derv1_gauss(self,l)
+        gauss_2d = np.zeros((l+1))
+        gauss_2d = np.convolve(gauss_1d,(1,-1))
+        print(gauss_2d)
+    
+    def low_step_block(self, n):
+        k_block = np.ones((n,n), dtype=int)/(n*n)
+        return k_block
+    
+    def convolution_2d(image, kernel):
+        row1, col1 = image.shape[0], image.shape[0]
+        row2, col2 = kernel.shape[0],kernel.shape[0]
+        result = [[0]*(col1 + col2 -1) for _ in range(row1+row2 -1)]
+        for i in range(row1):
+            for j in range(col1):
+                for k in range(row2):
+                    for l in range(col2):
+                        result[i + k][j + l] += image[i][j] * kernel[k][l]
+        
+        return result
+if __name__==  "__main__":
+    filtros = filters()
+    k_gauss = filtros.gauss_vector(3)
+    #print(k_gauss)
+    k_gauss = filtros.gauss_matrix(3)
+    print(k_gauss)
+    #k_dervgauss= filtros.derv2_gauss(3)
+    #print(k_dervgauss)
+    
+    image = cv2.imread('artecontemp.png', cv2.IMREAD_GRAYSCALE)
+    print(image.shape[0])
---
+++ Practicas/Practica4/Practica4/filters_applied.py
@@ -0,0 +1,156 @@
+import numpy as np
+import cv2
+import filters as fl
+class appliend_filters:
+    def __init__(self, image):
+        self.filtros = []
+        self.noise_img = cv2.blur(image,(9,9))
+        self.peper_noise_img = fl.filters.salt_pepper_noise(image,0.018)
+        
+        
+    def paso_bajas(self, img, n, img_name):
+        kernel = fl.filters.low_step_block(self,n)
+        blurred_image = cv2.filter2D(img, -1, kernel)
+        cv2.imwrite(f'./2/2_pasobajas_{n}_{img_name}.png',blurred_image)
+        return blurred_image
+    
+    def bin_paso_bajas(self, img, n, img_name):
+        kernel = fl.filters.gauss_matrix(self,n)
+        blurred_image = cv2.filter2D(img, -1, kernel)
+        cv2.imwrite(f'./3/3_binomial_{n}_{img_name}.png',blurred_image)
+        return blurred_image
+    
+    def edge_block(self, img, img_name):
+        kernel = np.array([-1,1])
+        #print(kernel)
+        #edge_detec = np.convolve(img.reshape(img.shape(0)*img.shape(1)),1,(1,-1))
+        edge_detec = cv2.filter2D(img, -1, kernel)
+        edge_detec_numpy = np.array(edge_detec)
+        #img_normalized = cv2.normalize(edge_detec_numpy, None, 0, 255, cv2.NORM_MINMAX)
+        cv2.imwrite(f'./4/4a_block_{img_name}.png',edge_detec_numpy)
+        #print(edge_detec)
+    
+    def Prewitt_x(self, img, img_name):
+        kernel = fl.filters.prewitt_x
+        edge_detec = fl.filters.convolution_2d(img,kernel)
+        edge_detec_numpy = np.array(edge_detec)
+        img_normalized = cv2.normalize(edge_detec_numpy, None, 0, 255, cv2.NORM_MINMAX)
+        cv2.imwrite(f'./4/4a_binomial_prewitt_{img_name}.png',img_normalized)
+        #print(img_normalized)
+        
+    def Prewitt_y(self, img, img_name):
+        kernel = fl.filters.prewitt_y
+        edge_detec = fl.filters.convolution_2d(img,kernel)
+        edge_detec_numpy = np.array(edge_detec)
+        img_normalized = cv2.normalize(edge_detec_numpy, None, 0, 255, cv2.NORM_MINMAX)
+        cv2.imwrite(f'./4/4a_binomial_prewitt_{img_name}.png',img_normalized)
+        
+    
+    def Sobel(self, img, img_name, flag):
+        if flag: 
+            kernel = fl.filters.sobel_x 
+        else: 
+            kernel = fl.filters.sobel_y
+            
+        print(kernel)
+        edge_detec = fl.filters.convolution_2d(img,kernel)
+        edge_detec_numpy = np.array(edge_detec)
+        img_normalized = cv2.normalize(edge_detec_numpy, None, 0, 255, cv2.NORM_MINMAX)
+        cv2.imwrite(f'./4/4c_binomial_sobel_{img_name}.png',img_normalized)
+        #print(img_normalized)
+        
+    def dervGauss(self, n, img, img_name):
+        kernel_bin = fl.filters.gauss_vector(self,n)
+        kernel = fl.filters.derv1_gauss(self, n)
+        kernel_mat = fl.filters.to_matrix(self,np.transpose(kernel_bin), kernel)
+        edge_detec = fl.filters.convolution_2d(img,kernel_mat)
+        edge_detec_numpy = np.array(edge_detec)
+        img_normalized = cv2.normalize(edge_detec_numpy, None, 0, 255, cv2.NORM_MINMAX)
+        cv2.imwrite(f'./4/4d_deriv_gauss_{img_name}_b_{n}.png',img_normalized)
+        #print(img_normalized)
+        
+    def laplace(self, img, img_name):
+        kernel = fl.filters.laplace
+        edge_detec = fl.filters.convolution_2d(img,kernel)
+        edge_detec_numpy = np.array(edge_detec)
+        img_normalized = cv2.normalize(edge_detec_numpy, None, 0, 255, cv2.NORM_MINMAX)
+        cv2.imwrite(f'./5/5a_laplace_{img_name}.png',img_normalized)
+    
+        
+    def derv2Gauss(self, n, img, img_name):
+        kernel = fl.filters.derv1_gauss(self, n)
+        kernel_mat = fl.filters.to_matrix(self,kernel,np.transpose(kernel))
+        edge_detec = fl.filters.convolution_2d(img,kernel_mat)
+        edge_detec_numpy = np.array(edge_detec)
+        img_normalized = cv2.normalize(edge_detec_numpy, None, 0, 255, cv2.NORM_MINMAX)
+        cv2.imwrite(f'./5/5b_2deriv_gauss_{img_name}_{n}.png',img_normalized)
+        #print(img_normalized)
+    
+if __name__==  "__main__":
+    #Load original image
+    image = cv2.imread('artecontemp.png', cv2.IMREAD_GRAYSCALE)
+    filters_applied = appliend_filters(image)
+    #imagen with noise
+    peper_img = filters_applied.peper_noise_img
+    
+    #cv2.imwrite(f'orign.png',image)
+    #cv2.imwrite(f'noise.png',peper_img)
+    
+    #Filters applied 3x3, 7x7, 9x9, 11x11
+    """ 
+    blurred_image = filters_applied.paso_bajas(image, 3, 'normal')
+    blurred_image_noise = filters_applied.paso_bajas(peper_img, 3, 'noise')
+    
+    blurred_image = filters_applied.paso_bajas(image, 7, 'normal')
+    blurred_image_noise = filters_applied.paso_bajas(peper_img, 7, 'noise')
+    
+    blurred_image = filters_applied.paso_bajas(image, 9, 'normal')
+    blurred_image_noise = filters_applied.paso_bajas(peper_img, 9, 'noise')
+    
+    blurred_image = filters_applied.paso_bajas(image, 11, 'normal')
+    blurred_image_noise = filters_applied.paso_bajas(peper_img, 11, 'noise') """
+    
+    #Binomial Filter 3x3, 7x7, 9x9, 11x11
+    """ blurred_img = filters_applied.bin_paso_bajas(image, 3, 'normal')
+    blurred_img_noise = filters_applied.bin_paso_bajas(peper_img, 3, 'noise')
+    
+    blurred_img = filters_applied.bin_paso_bajas(image, 7, 'normal')
+    blurred_img_noise = filters_applied.bin_paso_bajas(peper_img, 7, 'noise')
+    
+    blurred_img = filters_applied.bin_paso_bajas(image, 9, 'normal')
+    blurred_img_noise = filters_applied.bin_paso_bajas(peper_img, 9, 'noise')
+    
+    blurred_img = filters_applied.bin_paso_bajas(image, 11, 'normal')
+    blurred_img_noise = filters_applied.bin_paso_bajas(peper_img, 11, 'noise') """
+     
+    """ filters_applied.edge_block(image, 'normal_sinnorm')
+    filters_applied.edge_block(peper_img, 'noise_sinnorm') """
+    """ edge_img = filters_applied.Prewitt_y(image, 'normal_y')
+    edge_img = filters_applied.Prewitt_y(peper_img, 'noise_y') """
+    
+    """ edge_img = filters_applied.Sobel(image, 'normal_x', 1)
+    edge_img = filters_applied.Sobel(image, 'normal_y', 0)
+    edge_img = filters_applied.Sobel(peper_img, 'noise_x', 1)
+    edge_img = filters_applied.Sobel(peper_img, 'noise_y', 0) """
+    
+    """ filters_applied.dervGauss(5,image,'normal')
+    filters_applied.dervGauss(5,peper_img,'noise') """
+    
+    """ filters_applied.dervGauss(7,image,'normal')
+    filters_applied.dervGauss(7,peper_img,'noise')
+    filters_applied.dervGauss(11,image,'normal')
+    filters_applied.dervGauss(11,peper_img,'noise') """
+    
+    """ filters_applied.laplace(image, 'normal' )
+    filters_applied.laplace(peper_img, 'noise' )
+    
+    filters_applied.derv2Gauss(5,image,'normal')
+    filters_applied.derv2Gauss(5,peper_img,'noise')
+    filters_applied.derv2Gauss(7,image,'normal')
+    filters_applied.derv2Gauss(7,peper_img,'noise')
+    filters_applied.derv2Gauss(11,image,'normal')
+    filters_applied.derv2Gauss(11,peper_img,'noise')  """
+    
+    
+    
+    
